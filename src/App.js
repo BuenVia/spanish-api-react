0aace4293276fbf64843ce580458cece06be7bb6
SaracensFan <matthewclifford@hotmail.co.uk>
2022-11-28 11:04:30 +0000

    update

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Documentation from "./pages/Documentation";
+import Topics from "./pages/Topics";
+import NoPage from "./pages/NoPage";
+
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="documentation" element={<Documentation />} />
+          <Route path="topics" element={<Topics />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+export default function Header() {
+    return (
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="container-fluid">
+            <a class="n-brand" href="/">BuenVia API</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                <Link class="nav-link" to="/">Home</Link>
+                <Link class="nav-link" to="/documentation">Documentation</Link>
+                <Link class="nav-link" to="/topics">Topics</Link>
+                </div>
+            </div>
+            </div>
+        </nav>
+    )
+}
