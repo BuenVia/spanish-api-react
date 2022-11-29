8c235ca18e80edd728e4fc32a3d4d958a91ba623
Matthew Clifford <matthewclifford@hotmail.co.uk>
2022-11-29 08:17:11 +0000

    update

---
+++ src/components/CreateVocab.jsx
@@ -0,0 +1,81 @@
+import axios from "axios"
+import { useState } from "react"
+
+export default function CreateVocab(props) {
+
+    const [newVocab, setNewVocab] = useState({
+        topic: '',
+        eng: '',
+        esp: ''
+    })
+
+    function handleChange(e) {
+        const { name, value } = e.target
+        setNewVocab(prevVals => {
+            if(name === 'esp') {
+                return {
+                    ...prevVals,
+                    esp: value
+                }
+            } else if(name === 'eng') {
+                return {
+                    ...prevVals,
+                    eng: value
+                }
+            } else if(name === 'topic') {
+                return {
+                    ...prevVals,
+                    topic: value
+                }
+            }
+        })
+    }
+
+    // Add object to be sent
+    const sendVocab = async() => {
+        const { topic, eng, esp } = newVocab
+        if(topic === '' || eng === '' || esp === '') {
+            console.log('Plese fill in all fields')
+        } else {
+            const url = 'https://buenvia-api.onrender.com/api/vocab'
+            try {
+                // axios
+                // .post(url, {/*TO BE POPULATED*/})
+                // .then(res => console.log(res))
+                console.log(newVocab)
+            } catch (err) {
+                console.log(err)
+            }
+            setNewVocab(prevVals => {
+                return {
+                    topic: prevVals.topic,
+                    eng: '',
+                    esp: ''
+                }
+            })
+        }
+    }
+
+    return (
+        <div className="col-md-6 mb-3">
+            <div className="card">
+                <div className="card-header">
+                    <h4>Create Vocab</h4>
+                </div>
+                <div className="card-body">
+                    <select className="form-control mb-3" name="topic" onChange={handleChange}>
+                        <option>--SELECT--</option>
+                        {props.listOfTopics.map(topic => {
+                            return <option value={topic.name}>{topic.name}</option>
+                        })}
+                    </select>
+                    <input type="text" name='esp' className='form-control mb-3' onChange={handleChange} placeholder='Spanish' value={newVocab.esp}></input>
+                    <input type="text" name='eng' className='form-control mb-3' onChange={handleChange} placeholder='English' value={newVocab.eng}></input>
+                </div>
+                <div className="card-footer">
+                    <button className="btn btn-primary" onClick={sendVocab}>Submit</button>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import CreateVocab from "../components/CreateVocab";
 import LastCreated from "../components/LastCreated";
 import listOfTopics from "../listOfTopics";
 
@@ -27,26 +28,8 @@ export default function Admin() {
             <h1>Admin</h1>
             
             <div className="row">
-                <div className="col-md-6 mb-3">
-                    <div className="card">
-                        <div className="card-header">
-                            <h4>Create Vocab</h4>
-                        </div>
-                        <div className="card-body">
-                            <select className="form-control mb-3">
-                                {listOfTopics.map(topic => {
-                                    return <option>{topic.name}</option>
-                                })}
-                            </select>
-                            <input type="text" className='form-control mb-3' placeholder='Spanish'></input>
-                            <input type="text" className='form-control mb-3' placeholder='English'></input>
-                        </div>
-                        <div className="card-footer">
-                            <button className="btn btn-primary">Submit</button>
-                        </div>
-                    </div>
-                </div>
 
+                <CreateVocab listOfTopics={listOfTopics} />
                 <LastCreated latest={latestEntry} />
 
                 <div className="col-md-6 mb-3">
