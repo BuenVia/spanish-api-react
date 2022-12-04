be994a264c9dc0a2c23fd513b8f74dc77453c503
SaracensFan <matthewclifford@hotmail.co.uk>
2022-12-04 16:39:25 +0000

    final commit

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -4,22 +4,31 @@ import CreateVocab from "../components/CreateVocab";
 import LastCreated from "../components/LastCreated";
 import ReadVocab from "../components/ReadVocab";
 import listOfTopics from "../listOfTopics";
+import LoadingSpinner from '../components/LoadingSpinner'
 
 export default function Admin() {
 
     const [allEntries, setAllEntries] = useState([])
     const [latestEntry, setLatestEntry] = useState({})
+    const [loading, setLoading] = useState(false)
 
     const getAll = async () => {
-        const url = 'http://localhost:9000'
-        // const url = 'https://buenvia-api.onrender.com'
-        axios
-        .get(`${url}/api/vocab`)
-        .then(res => {
-            const last = res.data.length - 1
-            setLatestEntry(res.data[last])
-            setAllEntries(res.data)
-        })
+        try {
+        setTimeout(() => {
+            const url = 'http://localhost:9000'
+            // const url = 'https://buenvia-api.onrender.com'
+            axios
+            .get(`${url}/api/vocab`)
+            .then(res => {
+                const last = res.data.length - 1
+                setLatestEntry(res.data[last])
+                setAllEntries(res.data)
+            })
+            setLoading(true)
+        }, 1000) 
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     useEffect(() => {
@@ -32,13 +41,11 @@ export default function Admin() {
         <div className="container">
             <h1>Admin</h1>
             
-            <div className="row">
-
+            {loading ?             <div className="row">
                 <CreateVocab listOfTopics={listOfTopics} update={getAll} />
                 <LastCreated latest={latestEntry} />
                 <ReadVocab allEntries={allEntries} update={getAll} />
-
-            </div>
+            </div> : <LoadingSpinner /> }
         </div>
     )
 }
